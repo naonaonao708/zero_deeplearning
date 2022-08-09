060a680d2f4856e45689325e5b1c5818e45f9374
naonao708 <naonao708@outlook.com>
2022-08-09 21:06:02 +0900

    8/9

---
+++ func/layer.py
@@ -0,0 +1,23 @@
+from re import I, L
+import numpy as np
+import sys, os
+sys.path.append(os.pardir)
+from func.function import *
+
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+
+        return dx
