3a4efcdfe7ff7737035c4d9ac0cb2b9e841596ee
naonao708 <naonao708@outlook.com>
2022-08-10 20:22:46 +0900

    8/10

--- func/layer.py
+++ func/layer.py
@@ -1,5 +1,3 @@
-from re import I, L
-from turtle import forward
 import numpy as np
 import sys, os
 sys.path.append(os.pardir)
@@ -39,4 +37,45 @@ class Sigmoid:
 
         return dx
 
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        return dx
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+    
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+
+    def backward(self, dout = 1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+
+        return dx
 
